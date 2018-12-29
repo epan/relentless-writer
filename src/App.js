9d222e73d8b1706d598778446bd988106e03001b
Eric Pan <epan@users.noreply.github.com>
2018-12-29 15:58:15 -0800

    Connect textarea input to state

--- src/App.js
+++ src/App.js
@@ -2,13 +2,31 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: ""
+    };
+    this.handleInput = this.handleInput.bind(this);
+  }
+
+  handleInput(e) {
+    const text = e.target.value;
+    this.setState({ text });
+  }
+
   render() {
     return (
       <div className="App">
         <label>
           Don't stop writing for more than 5 seconds
           <br />
-          <textarea name="editor" />
+          <br />
+          <textarea
+            name="editor"
+            value={this.state.text}
+            onChange={this.handleInput}
+          />
         </label>
       </div>
     );
