8414733f9cd42323651e642f8fb4d073dd826755
Eric Pan <epan@users.noreply.github.com>
2018-12-29 16:49:27 -0800

    Add target typing time and min typing pause behavior

--- src/App.js
+++ src/App.js
@@ -6,34 +6,67 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      text: ""
+      text: "",
+      completed: false,
+      startedTyping: false
     };
+    this.MINIMUM_TYPING_PAUSE = 5000;
+    this.TARGET_TYPING_DURATION = 1000 * 10;
+    this.timeoutId = null;
+
+    this.startWritingCountdown = this.startWritingCountdown.bind(this);
     this.clearText = this.clearText.bind(this);
     this.handleInput = this.handleInput.bind(this);
-    this.clearText = debounce(this.clearText.bind(this), 5000);
+    this.clearText = debounce(
+      this.clearText.bind(this),
+      this.MINIMUM_TYPING_PAUSE
+    );
   }
 
   handleInput(e) {
+    // update user inputted text
     const text = e.target.value;
     this.setState({ text });
+
+    // start the debounced clearText timer
     this.clearText();
+
+    // start the total writing time target countdown once user starts typing
+    if (this.state.startedTyping === false) {
+      this.setState({ startedTyping: true });
+      this.startWritingCountdown();
+    }
   }
 
   clearText() {
-    this.setState({ text: "" });
+    // clear all the user inputted text and reset the writing time target
+    this.setState({ text: "", startedTyping: false });
+    clearTimeout(this.timeoutId);
+  }
+
+  startWritingCountdown() {
+    // start the writing countdown to prevent text from getting cleared
+    this.timeoutId = setTimeout(() => {
+      this.clearText = () => {};
+      this.setState({ completed: true });
+    }, this.TARGET_TYPING_DURATION);
   }
 
   render() {
     return (
       <div className="App">
         <label>
-          Don't stop writing for more than 5 seconds
-          <br />
-          <br />
+          {this.state.completed ? (
+            <p>You did it! Your text will not disappear.</p>
+          ) : (
+            <p>Don't stop writing for more than 5 seconds</p>
+          )}
           <textarea
             name="editor"
             value={this.state.text}
             onChange={this.handleInput}
+            rows="20"
+            cols="40"
           />
         </label>
       </div>
