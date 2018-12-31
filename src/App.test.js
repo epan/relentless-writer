f89559822851670156ddf72d4e2f484ece991ad0
Eric Pan <epan@users.noreply.github.com>
2018-12-30 22:11:14 -0800

    Swap Enzyme for react-testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { shallow } from "enzyme";
+import { render } from "react-testing-library";
 import App from "./App";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-it("renders without crashing", () => {
-  shallow(<App />);
+it("renders welcome message", () => {
+  const { getByText } = render(<App />);
+  expect(
+    getByText("Don't stop writing for more than 5 seconds")
+  ).toBeInTheDocument();
 });
